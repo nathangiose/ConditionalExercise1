22e92b51006238e97758c3beec9b127da0086f8d
nathangiose <nathangiose@gmail.com>
2021-12-24 01:41:14 +0200

    Add project files.

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 10;
+
+            if (choice > 1 && choice <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
